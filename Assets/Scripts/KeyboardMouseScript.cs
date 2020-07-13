1f61cc7c1b07f90191dd54f670fdc487d7277f66
konstantin <kosman.forgames@yandex.ru>
2020-07-13 22:40:04 +0300

    it's hard to be a dumbass
    запил на смену настроек управления, абсолютно жопный, потому что тема с интернета не совсем понятна, и не вяжется особо с тем что мне нужно
    а по-другому вроде вообще нельзя
    нада думоть

--- Assets/Scripts/KeyboardMouseScript.cs
+++ Assets/Scripts/KeyboardMouseScript.cs
@@ -8,6 +8,12 @@ public class KeyboardMouseScript : MonoBehaviour
     public Slider sensSlider;
     public Text sensLabel;
     public float mouseSensitivity;
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    public Text forward, backward, left, right, restart, exit;
+    public GameObject forwardChange, backwardChange, leftChange, rightChange, restartChange, ExitChange;
+    public string newKey;
+
+    public GameObject currentKey;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +24,17 @@ public class KeyboardMouseScript : MonoBehaviour
             sensSlider.value = PlayerPrefs.GetFloat("MouseSens");
         }
         sensLabel.text = (mouseSensitivity).ToString();
+
+        if (PlayerPrefs.HasKey("keyForward"))
+        {
+            forward.text = PlayerPrefs.GetString("keyForward");
+            backward.text = PlayerPrefs.GetString("keyBackward");
+            left.text = PlayerPrefs.GetString("keyLeft");
+            right.text = PlayerPrefs.GetString("keyRight");
+            restart.text = PlayerPrefs.GetString("keyRestart");
+            exit.text = PlayerPrefs.GetString("keyExit");
+        }
+        else FirstTime();
     }
 
     // Update is called once per frame
@@ -31,5 +48,52 @@ public class KeyboardMouseScript : MonoBehaviour
         mouseSensitivity = sensSlider.value;
         sensLabel.text = (mouseSensitivity/100).ToString();
         PlayerPrefs.SetFloat("MouseSens", sensSlider.value);
+    }
+
+    private void FirstTime()
+    {
+        keys.Add("Forward", KeyCode.W);
+        PlayerPrefs.SetString("keyForward",KeyCode.W.ToString());
+        keys.Add("Backward", KeyCode.S);
+        PlayerPrefs.SetString("keyBackward", KeyCode.S.ToString());
+        keys.Add("Left", KeyCode.A);
+        PlayerPrefs.SetString("keyLeft", KeyCode.A.ToString());
+        keys.Add("Right", KeyCode.D);
+        PlayerPrefs.SetString("keyRight", KeyCode.D.ToString());
+        keys.Add("Restart", KeyCode.F);
+        PlayerPrefs.SetString("keyRestart", KeyCode.F.ToString());
+        keys.Add("Exit", KeyCode.Escape);
+        PlayerPrefs.SetString("keyExit", KeyCode.Escape.ToString());
+    }
+
+    public void ChangeForward()
+    {
+        forwardChange.SetActive(true);
+        //forward = Input.GetKeyDown();
+        OnGUI();
+        //newKey = Input..ToString();
+        PlayerPrefs.SetString("keyForward",newKey);
+        forward.text = PlayerPrefs.GetString("keyForward");
+        //newKey = null;
+        forwardChange.SetActive(false);
+    }
+
+    void OnGUI()
+    {
+        if (currentKey != null)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                keys[currentKey.name] = e.keyCode;
+                currentKey.GetComponent<Text>().text = e.keyCode.ToString();
+                currentKey = null;
+            }
+        }
+    }
+
+    public void ChangeKey(GameObject clicked)
+    {
+        currentKey = clicked;
     }
 }
