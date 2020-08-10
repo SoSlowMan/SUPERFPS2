4378edd9463bf9b3954a9004c933d730a57edbdc
konstantin <kosman.forgames@yandex.ru>
2020-08-10 15:57:07 +0300

    рузке язык в режиме истории

--- Assets/Scripts/LocalizeLevelScript.cs
+++ Assets/Scripts/LocalizeLevelScript.cs
@@ -70,6 +70,39 @@ public class LocalizeLevelScript : MonoBehaviour
         RusStrings[24] = "АПОКАЛИПСИС НАЧАЛСЯ";
         RusStrings[25] = "ЛЮДИ ПЕПЛА УБИЛИ ТЕБЯ";
         RusStrings[26] = "На следующий уровень";
+        RusStrings[27] = "ПОБЕДА!";
+        RusStrings[28] = "На этом уровне";
+        RusStrings[29] = "Время:";
+        RusStrings[30] = "Лучшее время:";
+        RusStrings[31] = "Счет:";
+        RusStrings[32] = "Лучший счет:";
+        RusStrings[33] = "В режиме истории";
+        RusStrings[34] = "Лучший счет:";
+        RusStrings[35] = "Еще раз?";
+        RusStrings[36] = " Выйти из игры ";
+        RusStrings[37] = " Выйти в главное меню ";
+        RusStrings[38] = "Секретов найдено:";
+        RusStrings[39] = "Детей спасено:";
+        RusStrings[40] = "Врагов уничтожено:";
+        RusStrings[41] = "ПОБЕДА!";
+        RusStrings[42] = "На этом уровне";
+        RusStrings[43] = "Время:";
+        RusStrings[44] = "Лучшее время:";
+        RusStrings[45] = "Счет:";
+        RusStrings[46] = "Лучший счет:";
+        RusStrings[47] = "В режиме истории";
+        RusStrings[48] = "Время:";
+        RusStrings[49] = "Лучшее время:";
+        RusStrings[50] = "Счет:";
+        RusStrings[51] = "Лучший счет:";
+        RusStrings[52] = "Еще раз?";
+        RusStrings[53] = "На следующий уровень";
+        RusStrings[54] = "Выйти из игры";
+        RusStrings[55] = "Выйти в главное меню";
+        RusStrings[56] = "Секретов найдено:";
+        RusStrings[57] = "Детей спасено:";
+        RusStrings[58] = "Врагов уничтожено:";
+        RusStrings[59] = "Лучшее время:";
     }
 
     void createEngList()
@@ -101,11 +134,44 @@ public class LocalizeLevelScript : MonoBehaviour
         EngStrings[24] = "APOCALYPSE HAS STARTED";
         EngStrings[25] = "ASH PEOPLE KILLED YOU";
         EngStrings[26] = "Go to next level";
+        EngStrings[27] = "YOU WIN!";
+        EngStrings[28] = "On this level";
+        EngStrings[29] = "Your time:";
+        EngStrings[30] = "Your best time:";
+        EngStrings[31] = "Score:";
+        EngStrings[32] = "Best score:";
+        EngStrings[33] = "Story mode";
+        EngStrings[34] = "Best score:";
+        EngStrings[35] = "Try again?";
+        EngStrings[36] = "Return to Windows";
+        EngStrings[37] = "Return to Main Menu";
+        EngStrings[38] = "Secrets found:";
+        EngStrings[39] = "Kids saved:";
+        EngStrings[40] = "Enemies defeated:";
+        EngStrings[41] = "YOU WIN!";
+        EngStrings[42] = "On this level";
+        EngStrings[43] = "Your time:";
+        EngStrings[44] = "Your best time:";
+        EngStrings[45] = "Score:";
+        EngStrings[46] = "Best score:";
+        EngStrings[47] = "Story mode";
+        EngStrings[48] = "Your time:";
+        EngStrings[49] = "Your best time:";
+        EngStrings[50] = "Score:";
+        EngStrings[51] = "Best score:";
+        EngStrings[52] = "Try again?";
+        EngStrings[53] = "Go to next level";
+        EngStrings[54] = "Return to Windows";
+        EngStrings[55] = "Return to Main Menu";
+        EngStrings[56] = "Secrets found:";
+        EngStrings[57] = "Kids saved:";
+        EngStrings[58] = "Enemies defeated:";
+        EngStrings[59] = "Your best time:";
     }
 
     public void translateToRussian()
     {
-        for (int i = 0; i <= 26; i++)
+        for (int i = 0; i <= 59; i++)
         {
             Texts[i].text = RusStrings[i];
             Texts[i].font = rusFont;
@@ -114,7 +180,7 @@ public class LocalizeLevelScript : MonoBehaviour
 
     public void translateToEnglish()
     {
-        for (int i = 0; i <= 26; i++)
+        for (int i = 0; i <= 59; i++)
         {
             Texts[i].text = EngStrings[i];
             Texts[i].font = engFont;
